80550e5d9d5e62dcd7589eb1ed5b2ccdc1844da7
Armand van der Walt <amd@yotta.co.za>
2018-01-23 11:30:35 +0200

    Bug fix

--- ice3x-api/CreateNewOrderEntity.cs
+++ ice3x-api/CreateNewOrderEntity.cs
@@ -1,6 +1,13 @@
-﻿namespace ice3x_api
+﻿using Newtonsoft.Json;
+
+namespace ice3x_api
 {
     public class CreateNewOrderEntity
     {
+        [JsonProperty("order_id")]
+        public string OrderId { get; set; }
+
+        [JsonProperty("transaction_id")]
+        public string TransactionId { get; set; }
     }
 }
